7eec5852d87dbd1c1bf336c0851b3d0e95c7f71b
damiao-classroom <lgc928091232@163.com>
2019-08-31 19:03:30 +0800

    new

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const path = require('path');
 const PORT = process.env.PORT || 5002;
 const REDIS_PORT = process.env.PORT || 6379;
 
-const client = redis.createClient(REDIS_PORT);
+const client = redis.createClient({
+	port: REDIS_PORT,
+	password: 'wu5229485'
+});
 const app = express();
 
+
+// 注册模板
+app.set('views', './views'); // 指定视图所在的位置
+app.set('view engine', 'ejs'); // 注册模板引擎
 // 设置静态文件路径
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 // 缓存中间件
 function cache(req, res, next){
@@ -21,6 +29,26 @@ function cache(req, res, next){
 		if(err) throw err;
 		if(data != null){
 			res.send( `<h3>${username} has ${data} github repos.</h3>` )
+		}else{
+			next()
+		}
+	})
+}
+
+// 缓存仓库中间件
+function cacheRepos(req, res, next){
+	// 判断是否存在参数
+	if(!req.query['searchValue']){
+		res.render('index', { title: 'Github API', list: []});
+		return
+	}
+	const { searchValue } = req.query;
+
+	// 检测缓存中是否存在当前用户信息
+	client.get( searchValue+'-repos', (err, data) => {
+		if(err) throw err;
+		if(data != null){
+			res.render('index', { title: 'Github API', list: JSON.parse(data)});
 		}else{
 			next()
 		}
@@ -46,6 +74,30 @@ app.get('/repos/:username', async (req, res, next) => {
 		res.status(500)
 	}
 });
+
+
+// 测试返回页面
+app.use('/', cacheRepos);
+app.get('/', async (req, res, next) => {
+	// 接收参数
+	const { searchValue } = req.query
+	const response = await axios.get(`https://api.github.com/users/${searchValue}/repos`)
+	const result = response.data
+	let list = []
+	result.forEach(v => {
+		list.push({
+			reposName: v.full_name,
+			reposUrl: v.html_url,
+			lang: v.language,
+			fork: v.forks_count,
+			star: v.stargazers_count
+		})
+	})
+	// 把数据存入 Redis
+	client.set( searchValue+'-repos', JSON.stringify(list) );
+	res.render('index', { title: 'Github API', list});
+})
+
 
 
 app.listen(PORT, () => {
